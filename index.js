3a4dcd12b958fd435995d2fc0836dc84379aaf75
Dominique Chin-Jones <domecj13@gmail.com>
2022-10-12 10:15:28 -0400

    added answers Array

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ questionField.focus();
 const submitInput = document.querySelector("#submitInput");
 submitInput.addEventListener("click", shakeTheBall);
 
+const answers = [
+    "Yes",
+    "Certainly",
+    "Affirmative",
+    "No",
+    "Nope",
+    "Negative",
+    "Maybe",
+    "Perhaps",
+    "Very likely",
+    "Unlikely",
+    "Try asking again",
+    "Ask some other time",
+];
+
 function shakeTheBall() {
     const resultText = document.querySelector("#resultText p");
     resultText.textContent = "Ball has been shaken";
